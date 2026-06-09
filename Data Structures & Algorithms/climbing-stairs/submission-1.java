c65bf1c798eb3116bd37d580280742cfbd1dd426
SaiTeja0556 <237r1a0556@cmrtc.ac.in>
2026-06-09 10:25:56 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return help(n,dp);
+    }
+
+    public int help(int n,int[] dp){
+        if(n < 0){
+            return 0;
+        }
+
+        if(n == 0){
+            return 1;
+        }
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = help(n-1,dp) + help(n-2,dp);
+    }
+}
